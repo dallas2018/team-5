f2e31dc7bbf52eae423c8d7c79a4d07ae817beef
Liang Gao <liang@gao.nyc>
2018-10-06 04:02:32 -0500

    trying out vision api'

--- server.js
+++ server.js
@@ -3,8 +3,9 @@ const express = require('express')
 const app = express()
 const port = process.env.PORT || 3000;
 const admin = require('firebase-admin');
+const vision = require('@google-cloud/vision');
 
-const serviceAccount = require(GOOGLE_SA);
+const serviceAccount = require(process.env.GOOGLE_APPLICATION_CREDENTIALS);
 
 admin.initializeApp({
     credential: admin.credential.cert(serviceAccount),
@@ -19,6 +20,7 @@ app.get('/', function (req, res) {
     res.send('Hello World!')
 })
 
+// Returning all the documents in the listings collection
 app.get('/listings/all', function (req, res) {
     var listings = {};
     db.collection('listings').get()
@@ -34,5 +36,94 @@ app.get('/listings/all', function (req, res) {
             res.send('{}');
         });
 })
+
+const Multer = require('multer');
+const bucket = admin.storage().bucket();
+
+const multer = Multer({
+    storage: Multer.memoryStorage(),
+    limits: {
+        fileSize: 5 * 1024 * 1024
+    }
+});
+
+// Adding new file to the firebase storage
+app.post('/upload', multer.single('file'), function (req, res) {
+    console.log('Upload Image');
+
+    const filename = req.file.originalname;
+    const fileupload = bucket.file(filename);
+    const uploadStream = fileupload.createWriteStream({
+        metadata: {
+            contentType: file.mimetype
+        }
+    });
+    uploadStream.on('error', (err) => {
+        console.log(err);
+        return;
+    });
+
+    uploadStream.on('finish', () => {
+        console.log('Upload success');
+    });
+
+    uploadStream.end(file.buffer);
+    // const blob = bucket.file(req.file.originalname);
+    // const blobStream = blob.createWriteStream({
+    //     metadata: {
+    //         contentType: req.file.mimetype
+    //     }
+    // });
+
+    // blobStream.on("error", err => {
+    // });
+    // blobStream.on("finish", () => {
+    // });
+    
+    // let filename = req.file.originalname;
+    // console.log(req.protocol + req.file.path);
+    // console.log(filename);
+
+    // bucket.upload(filename).then(() => {
+    //     console.log(`File ${filename} uploaded to gs`);
+    // })
+    // .catch(err => {
+    //     console.error('ERROR:', err);
+    // });
+
+    // if (filename) {
+    //     bucket.upload(filename, {
+    //         gzip: true,
+    //         metadata: {
+    //             cacheControl: 'public, max-age=31536000',
+    //         },
+    //     })
+    //     .then(() => {
+    //         console.log(`${filename} uploaded to bucket.`);
+    //     })
+    //     .catch(err => {
+    //         console.error('ERROR:', err);
+    //     });
+    // }
+});
+
+app.get('/label', function (req, res) {
+    const bucketName = 'jpmc4g-18.appspot.com';
+    const fileName = 'test/swell.jpg';
+    const client = new vision.ImageAnnotatorClient();
+    client
+        .labelDetection(`gs://${bucketName}/${fileName}`)
+        .then(results => {
+            const labels = results[0].labelAnnotations;
+
+            console.log('Labels:');
+            labels.forEach(label => console.log(label.description));
+        })
+        .catch(err => {
+            console.error('ERROR:', err);
+        });
+})
+
+
 
 app.listen(port, () => console.log(`Express server is listening on port ${port}!`))
