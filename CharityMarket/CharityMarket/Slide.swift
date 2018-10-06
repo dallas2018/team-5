67b4fd5ed594c6f78fa145d54e56dc042cc6851c
Bernardo Bustamante-Martinez <bbustam3@asu.edu>
2018-10-06 00:28:24 -0700

    Before merge

---
+++ CharityMarket/CharityMarket/Slide.swift
@@ -0,0 +1,22 @@
+//
+//  Slide.swift
+//  CharityMarket
+//
+//  Created by Bernardo Bustamante on 10/5/18.
+//  Copyright © 2018 Zak Wegweiser. All rights reserved.
+//
+
+import UIKit
+
+class Slide: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+    @IBOutlet weak var descLabel: UITextView!
+    
+}
